ce06f32df5ea8212f0e6ca5f90c52568e5e2c6d5
SteGarAla <stegarc@ut.utm.edu>
2023-02-17 18:33:40 -0600

    Add project files.

---
+++ ClassWork7/Program.cs
@@ -0,0 +1,92 @@
+﻿namespace ClassWork7
+{
+   
+
+    //pass
+    interface Widget
+    {
+        public void draw();
+    }
+
+    //pass
+    class TextField : Widget
+    {
+        private int width;
+        private int height;
+
+        public TextField(int w, int h)
+        {
+            width = w; height = h;
+        }
+        public void draw()
+        {
+            Console.WriteLine($"TextField: {width}, {height}");
+        }
+    }
+
+    //pass
+    abstract class Decorator : Widget
+    {
+        private Widget wid;
+
+        public Decorator(Widget wid)
+        {
+            this.wid = wid;
+        }
+
+        public void draw()
+        {
+            wid.draw();
+        }
+    }
+
+
+
+    class BorderDecorator : Decorator
+    {
+        public BorderDecorator(Widget w)
+        {
+            //something
+        }
+
+        override void draw()
+        {
+            base.draw();
+            Console.WriteLine("I am the BorderDecorator!");
+        }
+
+    }
+
+    class ScrollDecorator : Decorator
+    {
+        ScrollDecorator(Widget w)
+        {
+            //something
+        }
+
+        public void draw()
+        {
+            base.draw();
+            Console.WriteLine("I am the ScrollDecorator!");
+        }
+
+
+
+        class DecoratorDemo
+        {
+            static void Main()
+            {
+                TextField obj = new ScrollDecorator(); 
+            }
+        }
+
+    }
+
+
+
+
+
+
+
+
+}
